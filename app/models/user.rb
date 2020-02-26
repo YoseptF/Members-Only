30022a891424c4f6a77c901a0dd5551e7b2799bf
NewIncome <alfredo_cardenas@live.com>
2020-02-26 17:00:50 -0600

    Add some validations for the user and the bcrypt gem in the Gemfile for the secure password availabilty.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+  
+  validate :username, presence: true, length: { in: 6..15 }
+  validate :email, presence: true, length: { max: 40 },
+                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze }
+  validate :password, presence: true, length: { in: 6..15 }
+
+  has_secure_password
 end
